13df15ee53d3ea177044b86158f33b693ace2199
Mbadiwe Nnaemeka Ronald <ron2tele@gmail.com>
2019-08-23 10:02:01 +0100

    Visual Studio project file

---
+++ SRL App.cpp
@@ -0,0 +1,25 @@
+// ///////////////////////////////////////////////////////////////// //
+// *SRL-C++ 11 Library                                 
+// *Copyright(c) 2019  Mbadiwe Nnaemeka Ronald                 
+// *Github Repository <https://github.com/ron4fun>             
+
+// *Distributed under the MIT software license, see the accompanying file LICENSE 
+// *or visit http ://www.opensource.org/licenses/mit-license.php.           
+
+// *Acknowledgements:                                  
+// ** //
+// *Thanks to Simple Regex (https://simple-regex.com/) for his creative        
+// *development of this library in Javascript                         
+
+// ////////////////////////////////////////////////////// ///////////////
+
+#define BOOST_TEST_MODULE SRLTestCase
+
+#include "Tests\ParseParenthesesTests.h"
+#include "Tests\BuilderTests.h"
+#include "Tests\Issue_11_Tests.h"
+#include "Tests\ExceptionsTests.h"
+#include "Tests\InterpreterTests.h"
+#include "Tests\RuleTests.h"
+
+
